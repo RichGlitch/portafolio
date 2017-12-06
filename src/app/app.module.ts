9071c27c35a3ee745dd9b3ace8937e569b340432
Ricardo Velasco <ricardo.pro@outlook.com>
2017-12-06 14:24:13 -0600

    about agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
+import { AboutComponent } from './components/about/about.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { MainComponent } from './components/main/main.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
